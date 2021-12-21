886cb8c8f10fa3e2fbd0c9314340799e6ff8256f
李超 <1246877006@qq.com>
2021-12-21 23:35:31 +0800

    requestMapping的学习

--- springMVC-demo2/src/main/java/com/atguigu/mvc/controller/RequestMappingTest.java
+++ springMVC-demo2/src/main/java/com/atguigu/mvc/controller/RequestMappingTest.java
@@ -1,14 +1,40 @@
 package com.atguigu.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "hello")
 public class RequestMappingTest {
 
-    @RequestMapping(value = {"target", "test"})
+    @RequestMapping(
+            value = {"target", "test"},
+            method = {RequestMethod.GET, RequestMethod.POST}
+    )
     public String toTarget() {
+        return "target";
+    }
+
+    @RequestMapping(
+            value = "/testParamsAndHeader",
+            params = {"username"}
+    )
+    public String testParamsAndHeader() {
+        return "target";
+    }
+
+    //@RequestMapping("/a?a/testAnt")
+    //@RequestMapping("/a*a/testAnt")
+    @RequestMapping("/**/testAnt")
+    public String testAnt() {
+        return "target";
+    }
+
+    @RequestMapping("testPath/{id}")
+    public String testPath(@PathVariable("id")Integer id) {
+        System.out.println("id：" + id);
         return "target";
     }
 }
