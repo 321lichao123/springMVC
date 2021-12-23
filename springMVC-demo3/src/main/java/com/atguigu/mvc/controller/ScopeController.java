419fc5d27f074e67c0de9581c6f78f125192e240
lichao <1246877006@qq.com>
2021-12-23 18:00:19 +0800

    域对象共享数据的学习

---
+++ springMVC-demo3/src/main/java/com/atguigu/mvc/controller/ScopeController.java
@@ -0,0 +1,56 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+@Controller
+public class ScopeController {
+
+    @RequestMapping("/testRequestServletAPI")
+    public String testRequestServletAPI(HttpServletRequest request) {
+        request.setAttribute("testRequestScope", "hello servletAPI");
+        return "success";
+    }
+
+    @RequestMapping("/testModelAndView")
+    public ModelAndView testModelAndView() {
+        ModelAndView mav = new ModelAndView();
+        // 处理模型数据， 即向request域对象中存储数据
+        mav.addObject("testRequestScope", "hello, modelAndView");
+        // 设置试图名称
+        mav.setViewName("success");
+        return mav;
+    }
+
+    @RequestMapping("/testModel")
+    public String testModel(Model model) {
+        model.addAttribute("testRequestScope", "hello, Model");
+        return "success";
+    }
+
+    @RequestMapping("/testMap")
+    public String testMap(Map<String, Object> map) {
+        map.put("testRequestScope", "hello, map");
+        return "success";
+    }
+
+    @RequestMapping("/testModelMap")
+    public String testModelMap(ModelMap modelMap) {
+        modelMap.addAttribute("testRequestScope", "hello, ModelMap");
+        return "success";
+    }
+
+    @RequestMapping("/testSession")
+    public String testSession(HttpSession session) {
+        session.setAttribute("testSeesionScope", "hello,Session");
+        return "success";
+    }
+}
