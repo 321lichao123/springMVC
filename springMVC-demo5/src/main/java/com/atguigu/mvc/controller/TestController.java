084f0476996643fded7c311af554887aecd4e224
李超 <1246877006@qq.com>
2021-12-29 00:13:26 +0800

    拦截器的学习

---
+++ springMVC-demo5/src/main/java/com/atguigu/mvc/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName TestController
+ * @Description
+ * @Author 12468
+ * @Date 2021/12/28 23:16
+ * @Version 1.0
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/testInterceptor")
+    public String testInterceptor() {
+        return "success";
+    }
+}
