3098507eee4e342e6e7ab88f581ca42dc047f8e1
lichao <1246877006@qq.com>
2021-12-29 16:08:46 +0800

    注解配置springMVC

---
+++ springMVC-demo5/src/main/java/com/atguigu/mvc/controller/ExceptionController.java
@@ -0,0 +1,15 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(value = {ArithmeticException.class, NullPointerException.class})
+    public String testException(Exception exception, Model model) {
+        model.addAttribute("ex", exception);
+        return "error";
+    }
+}
